f68ea8f921f03f87f0625fc034e83621981657e2
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-02-23 17:04:44 +0100

    tar in svar från spelare och dator

--- Program.cs
+++ Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
+        static bool IsValid(string str)
+        {
+            if (str == "sten" || str == "sax" || str == "påse")
+            {
+                return true;
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till Sten, Sax eller Påse! Lycka till!");
+            string player;
+
+            // REGISTRERAR DRAG FRÅN SPELAREN
+            Console.Write("Din tur! välj sten, sax eller påse: ");
+            do {
+                player = Console.ReadLine().ToLower();
+                if (IsValid(player))
+                {
+                    Console.WriteLine($"Du valde {player}");
+                } else
+                {
+                    Console.Write("Ogiltligt svar, välj sten, sax eller påse: ");
+                }
+            } while(!IsValid(player));
+
+            // REGISTRERAR DRAG FRÅN DATORN
+            Random random = new();
+            string computer = Convert.ToString(random.Next(1,3));
+
+            switch (computer) 
+            {
+                case "1":
+                    computer = "sten";
+                    break;
+                case "2":
+                    computer = "sax";
+                    break;
+                case "3":
+                    computer = "påse";
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine(player + " " + computer);
         }
     }
 }
