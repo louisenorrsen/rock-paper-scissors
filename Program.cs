b8814e2dad961752bef78cc5367ba5477412e69a
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-02-23 17:32:07 +0100

    bryter spelet när önskad poäng uppnåtts

--- Program.cs
+++ Program.cs
@@ -12,42 +12,57 @@
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till Sten, Sax eller Påse! Lycka till!");
+            Console.WriteLine("Välkommen till Sten, Sax eller Påse! Lycka till!\n");
             string player;
-
-            // REGISTRERAR DRAG FRÅN SPELAREN
-            Console.Write("Din tur! välj sten, sax eller påse: ");
+            int playerPoints = 0;
+            int computerPoints = 0;
             do {
-                player = Console.ReadLine().ToLower();
-                if (IsValid(player))
+                // REGISTRERAR DRAG FRÅN SPELAREN
+                Console.Write("Din tur! välj sten, sax eller påse: ");
+                do
                 {
-                    Console.WriteLine($"Du valde {player}");
+                    player = Console.ReadLine().ToLower();
+                    if (IsValid(player))
+                    {
+                        Console.WriteLine($"Du valde {player}");
+                    }
+                    else
+                    {
+                        Console.Write("Ogiltligt svar, välj sten, sax eller påse: ");
+                    }
+                } while (!IsValid(player));
+
+                // REGISTRERAR DRAG FRÅN DATORN
+                Random random = new();
+                string computer = Convert.ToString(random.Next(1, 3));
+
+                switch (computer)
+                {
+                    case "1":
+                        computer = "sten";
+                        break;
+                    case "2":
+                        computer = "sax";
+                        break;
+                    case "3":
+                        computer = "påse";
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine($"Datorn valde {computer}");
+
+                // AVGÖR VEM SOM VANN RUNDAN
+                if (player == "sten" && computer == "sax" || player == "sax" && computer == "påse" || player == "påse" && computer == "sten")
+                {
+                    Console.WriteLine("Ett poäng till dig!");
+                    playerPoints++;
                 } else
                 {
-                    Console.Write("Ogiltligt svar, välj sten, sax eller påse: ");
+                    Console.WriteLine("Ett poäng till datorn!");
+                    computerPoints++;
                 }
-            } while(!IsValid(player));
-
-            // REGISTRERAR DRAG FRÅN DATORN
-            Random random = new();
-            string computer = Convert.ToString(random.Next(1,3));
-
-            switch (computer) 
-            {
-                case "1":
-                    computer = "sten";
-                    break;
-                case "2":
-                    computer = "sax";
-                    break;
-                case "3":
-                    computer = "påse";
-                    break;
-                default:
-                    break;
-            }
-
-            Console.WriteLine(player + " " + computer);
+            } while (playerPoints < 2 && computerPoints < 2);
         }
     }
 }
